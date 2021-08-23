a9354d0bed3500dfe02baacf3b0ea9aefadbb667
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-08-23 02:39:37 -0300

    Feat: Limpeza de código

--- js/keyboard.js
+++ js/keyboard.js
@@ -3,10 +3,7 @@ class Keyboard {
     window.addEventListener('keydown', (event) => this.handler(event));
     window.addEventListener('keyup', (event) => this.handler(event));
     this.map = {
-      arrowup: false,
-      arrowleft: false,
-      arrowright: false,
-      arrowdown: false,
+      arrowup: false, arrowleft: false, arrowright: false, arrowdown: false,
     };
   }
 
@@ -15,7 +12,6 @@ class Keyboard {
    * @param {KeyboardEvent} event
    */
   handler(event) {
-    // console.log(event.type);
     const key = event.key.toLowerCase();
     this.map[key] = event.type === 'keydown';
   }
