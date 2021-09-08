c2c7a5294c87525b452a1069de95f7031ab742ba
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-09-08 01:34:26 -0300

    refactor: improves tunnel render

--- js/tunnel.js
+++ js/tunnel.js
@@ -1,12 +1,47 @@
 class Tunnel {
-  constructor() {
-    this.offsetX = 0;
-    this.title = '';
-  }
+  /**
+   * @type {String}
+   */
+  title;
 
-  update() {
-    console.log('tunnel', this.offsetX);
-  }
+  /**
+   * @type {Number}
+   */
+  py;
+
+  /** 
+   * @type {Number}
+   */
+  clipH;
+
+  /** 
+   * @type {Number}
+   */
+  worldH;
+  leftFace = new class {
+    offsetX1 = 1.3;
+    offsetX2 = 1.3;
+  };
+  rightFace = new class {
+    offsetX1 = 1.3;
+    offsetX2 = 1.3;
+  };
+
+  visibleFaces = new class {
+    leftFront = true;
+    rightFront = true;
+    centerFront = true;
+    leftTop = true;
+    rightTop = true;
+    centerTop = true;
+    leftCover = true;
+    rightCover = true;
+  };
+
+  /**
+   * @type {SegmentLine}
+   */
+  previousSegment;
 }
 
 export default Tunnel;
