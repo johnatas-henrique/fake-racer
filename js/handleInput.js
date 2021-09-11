9ba9d713a5f055e58e6370f4d93b2036280c97ef
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-09-11 15:12:51 -0300

    Add: Botões touch para dispositivos móveis

--- js/handleInput.js
+++ js/handleInput.js
@@ -1,7 +1,13 @@
 class HandleInput {
   constructor() {
+    const buttons = document.querySelectorAll('button');
     window.addEventListener('keydown', (event) => this.handler(event));
     window.addEventListener('keyup', (event) => this.handler(event));
+    buttons.forEach((button) => {
+      button.addEventListener('contextmenu', (event) => event.preventDefault());
+      button.addEventListener('touchstart', (event) => this.handler(event));
+      button.addEventListener('touchend', (event) => this.handler(event));
+    });
     this.map = {
       arrowup: false, arrowleft: false, arrowright: false, arrowdown: false,
     };
@@ -12,8 +18,14 @@ class HandleInput {
    * @param {KeyboardEvent} event
    */
   handler(event) {
-    const key = event.key.toLowerCase();
-    this.map[key] = event.type === 'keydown';
+    if (event.type === 'keyup' || event.type === 'keydown') {
+      const key = event.key.toLowerCase();
+      this.map[key] = event.type === 'keydown';
+    }
+    if (event.type === 'touchstart' || event.type === 'touchend') {
+      const key = event.target.name;
+      this.map[key] = event.type === 'touchstart';
+    }
   }
 
   isKeyDown(key) {
