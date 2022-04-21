1567d8345db20105b993135ee63b5ee6d556c8ae
Johnatas Henrique <johnatas.henrique@gmail.com>
2022-04-21 15:38:42 -0300

    feat: create the Person extended class

--- js/GameObject.js
+++ js/GameObject.js
@@ -2,9 +2,14 @@ class GameObject {
   constructor(config) {
     this.x = config.x || 0;
     this.y = config.y || 0;
+    this.direction = config.direction || 'down';
     this.sprite = new Sprite ({
       gameObject: this,
       src: config.src || '../assets/images/characters/people/hero.png',
     });
+  }
+
+  update() {
+
   }
 }
---
+++ js/Person.js
@@ -0,0 +1,30 @@
+class Person extends GameObject {
+  constructor(config) {
+    super(config);
+    this.movingProgressRemaining = 0;
+    this.direction = 'right';
+    this.directionUpdate = {
+      'up': ['y', -1],
+      'down': ['y', 1],
+      'left': ['x', -1],
+      'right': ['x', 1],
+    }
+  }
+
+  update(state) {
+    this.updatePosition();
+    if (this.movingProgressRemaining === 0 && state.arrow) {
+      this.direction = state.arrow;
+      this.movingProgressRemaining = 16;
+    }
+  }
+
+  updatePosition() {
+    if (this.movingProgressRemaining > 0) {
+      const [property, change] = this.directionUpdate[this.direction];
+      this[property] += change;
+      this.movingProgressRemaining -= 1;
+    }
+  }
+
+}
