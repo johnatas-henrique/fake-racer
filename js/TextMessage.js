d990331c4730a1dbc793a2a3ec6473314568d940
Johnatas Henrique <johnatas.henrique@gmail.com>
2022-04-26 01:20:29 -0300

    feat: add typewriter effect to message texts

--- js/TextMessage.js
+++ js/TextMessage.js
@@ -10,28 +10,38 @@ class TextMessage {
     this.element.classList.add('TextMessage');
 
     this.element.innerHTML = (`
-      <p class='TextMessage_p'>${this.text}</p>
+      <p class='TextMessage_p'></p>
       <button class='TextMessage_button'>Next</button>
     `);
+
+    this.revealingText = new RevealingText({
+      element: this.element.querySelector('.TextMessage_p'),
+      text: this.text,
+    });
 
     this.element.querySelector('button').addEventListener('click', () => this.done());
 
     this.actionListener = new KeyPressListener({
       keyCode: 'Enter',
       callback: () => {
-        this.actionListener.unbind();
         this.done();
       },
     });
   };
 
   done() {
-    this.element.remove();
-    this.onComplete();
-  }
+    if (this.revealingText.isDone) {
+      this.element.remove();
+      this.actionListener.unbind();
+      this.onComplete();
+    } else {
+      this.revealingText.warpToDone();
+    }
+  };
 
   init(container) {
     this.createElement();
     container.appendChild(this.element);
+    this.revealingText.init();
   };
 };
