e209ed3e8f06318c127e0de00a569093e6bc2f50
Johnatas Henrique <johnatas.henrique@gmail.com>
2023-01-23 00:29:01 -0300

    refactor: redone SpriteRace to receive an object with all parameters

--- js/Race/SpriteRace.js
+++ js/Race/SpriteRace.js
@@ -1,16 +1,18 @@
 class SpriteRace {
-  constructor() {
-    this.image = {};
-    this.offsetX = 0;
-    this.offsetY = 0;
-    this.sheetPositionX = 0;
-    this.sheetPositionY = 0;
-    this.scaleX = 1;
-    this.scaleY = 1;
-    this.spritesInX = 1;
-    this.spritesInY = 1;
-    this.name = 'sprite';
+  constructor(config = {}) {
+    this.image = new Image();
+    this.offsetX = config.offsetX || 0;
+    this.offsetY = config.offsetY || 0;
+    this.sheetPositionX = config.sheetPositionX || 0;
+    this.sheetPositionY = config.sheetPositionY || 0;
+    this.scaleX = config.scaleX || 1;
+    this.scaleY = config.scaleY || 1;
+    this.spritesInX = config.spritesInX || 1;
+    this.spritesInY = config.spritesInY || 1;
+    this.name = config.name || 'sprite';
     this.actualSpeed = { speed: 0, mult: 0 };
+    const transparentImage = '../assets/images/clean.png';
+    this.image.src = config.imageSrc || transparentImage;
   }
 
   get width() {
