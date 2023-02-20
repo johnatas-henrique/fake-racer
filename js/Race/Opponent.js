f49c90a03b53b8904aa9611f649b5d62785b7229
Johnatas Henrique <johnatas.henrique@gmail.com>
2023-02-20 05:04:06 -0300

    refactor: improve collision detection of Opponent's class

--- js/Race/Opponent.js
+++ js/Race/Opponent.js
@@ -10,6 +10,11 @@ class Opponent {
     this.opponentName = config.opponentName || 'Desconhecido';
     this.carColor = config.carColor ?? 'random';
     this.image = config.image || '/assets/images/cars/f1Y91Opponents.png';
+    this.actualSpeed = 0;
+    this.opponentX = 1;
+    this.lap = 0;
+    this.raceTime = [0];
+    this.isCrashed = 0;
     this.sprite = new SpriteRace({
       offsetX: 0.5 * this.startPos,
       scaleX: 2,
@@ -19,35 +24,10 @@ class Opponent {
       imageSrc: this.image,
       name: `op${this.opponentName}`,
     });
-    this.actualSpeed = 0;
-    this.scale = 0;
-    this.opponentX = 1;
-    this.lap = 0;
-    this.raceTime = [0];
-    this.isCrashed = 0;
-    this.images = {};
-    this.road = null;
-    this.director = null;
-    this.player = null;
-    this.opponents = null;
   }
 
   init() {
-    this.road = this.race.road;
-    this.director = this.race.director;
-    this.player = this.race.player;
-    this.opponents = this.race.opponents;
-    this.images.opponents = new Image();
-    this.images.opponents.src = this.image;
-    this.sprite.image = this.images.opponents;
-    this.sprite.name = `op${this.opponentName}`;
-    this.sprite.offsetX = 0.5 * this.startPos;
-    this.sprite.scaleX = 2;
-    this.sprite.scaleY = 2;
-    this.sprite.spritesInX = 8;
-    this.sprite.spritesInY = 1;
     this.sprite.sheetPositionX = this.carColor !== 'random' ? this.carColor : Math.floor(Math.random() * 7.99);
-    this.sprite.sheetPositionY = 0;
     this.sprite.actualSpeed.mult = 1;
     this.maxSpeed += Math.floor(Math.random() * 20);
     this.baseSpeed = this.maxSpeed;
@@ -64,11 +44,72 @@ class Opponent {
   }
 
   findFinishedCars(driver) {
-    return this.director.raceFinishedCars.find(({ name }) => name === driver);
+    return this.race.director.raceFinishedCars.find(({ name }) => name === driver);
+  }
+
+  updateAxisX() {
+    if (this.sprite.offsetX <= -1.65 / 2) this.opponentX = 1;
+    if (this.sprite.offsetX >= 1.65 / 2) this.opponentX = -1;
+  }
+
+  updateOpponentsCarOffset() {
+    const playerParam = this.race.player;
+    const oppArrParam = this.race.opponents;
+    const lookAhead = 80;
+    const crash = 4;
+    const { carSegments } = this.race.director;
+    const cSeg = carSegments.find(({ name }) => name === this.opponentName);
+    const arrObjSeg = carSegments.filter(({ pos }) => pos < cSeg.pos + lookAhead && pos > cSeg.pos);
+    const objCrash = carSegments.find(({ pos }) => pos < cSeg.pos + crash && pos > cSeg.pos);
+
+    arrObjSeg.forEach((objSeg) => {
+      if (objSeg && objSeg.name !== playerParam.name && !this.isCrashed) {
+        const isOverlapped = utils.overlap(cSeg.x, 0.663125, objSeg.x, 0.663125, 0.9);
+
+        if (isOverlapped) {
+          const diffCarsX = Math.abs(objSeg.x - cSeg.x);
+          if (Math.abs(objSeg.x) > 1 || (Math.abs(objSeg.x) > 0 && diffCarsX < 0)) {
+            this.opponentX = objSeg.x * -1;
+          }
+
+          if (objCrash && objCrash.name !== playerParam.name && !this.isCrashed) {
+            const opp = oppArrParam.findIndex(({ opponentName }) => opponentName === objCrash.name);
+            oppArrParam[opp].actualSpeed *= 1.04;
+            this.actualSpeed *= 0.96;
+            this.isCrashed = 1;
+          }
+        }
+      }
+      if (objSeg && objSeg.name === playerParam.name) {
+        const playerW = playerParam.sprite.scaleX * 320 * 0.001;
+        const isOverlapped = utils.overlap(cSeg.x, 0.663125, objSeg.x, playerW, 1);
+
+        if (isOverlapped/*  && this.actualSpeed > playerParam.actualSpeed */) {
+          const diffCarsX = Math.abs(objSeg.x - cSeg.x);
+          if (objSeg.x > 0.95 || (objSeg.x > 0 && diffCarsX < 0.4)) {
+            this.opponentX = -20;
+          } else if (objSeg.x < -0.95 || (objSeg.x < 0 && diffCarsX < 0.4)) {
+            this.opponentX = 20;
+          }
+
+          if (objCrash && !this.isCrashed) {
+            const crashSpeeds = {};
+            crashSpeeds.player = playerParam.actualSpeed;
+            crashSpeeds.opponent = this.actualSpeed;
+            crashSpeeds.midpoint = (crashSpeeds.player + crashSpeeds.opponent) / 2;
+            crashSpeeds.newPlayer = crashSpeeds.midpoint * 1.1;
+            crashSpeeds.newOpponent = crashSpeeds.midpoint * 0.9;
+            playerParam.actualSpeed = crashSpeeds.newPlayer;
+            this.actualSpeed = crashSpeeds.newOpponent;
+            this.isCrashed = 1;
+          }
+        }
+      }
+    });
   }
 
   update() {
-    if (this.director.running) {
+    if (this.race.director.running) {
       const newMaxSpeed = this.carSpeedCorrection();
       newMaxSpeed.next();
       if (this.actualSpeed < 0) this.actualSpeed = 0;
@@ -80,24 +121,25 @@ class Opponent {
         ? this.maxSpeed : this.actualSpeed += acceleration(this.actualSpeed, 0.9);
 
       this.sprite.actualSpeed.speed = this.actualSpeed;
-      const oldSegment = this.road.getSegment(Math.round(this.trackPosition));
-      this.opponentX = utils
-        .updateOpponentsCarOffset(this, this.player, this.director, this.opponents);
+      const oldSegment = this.race.road.getSegment(Math.round(this.trackPosition));
+      this.updateAxisX();
+      this.updateOpponentsCarOffset();
 
       this.sprite.offsetX += 0.008 * this.opponentX;
       this.trackPosition += this.actualSpeed;
-      this.isCrashed = this.isCrashed > 0 ? this.isCrashed -= 0.1 : 0;
-      const actualSegment = this.road.getSegment(Math.round(this.trackPosition));
+      this.isCrashed = this.isCrashed > 0 ? this.isCrashed -= 0.01 : 0;
+
+      const actualSegment = this.race.road.getSegment(Math.round(this.trackPosition));
       oldSegment.sprites = oldSegment.sprites.filter(({ name }) => name !== this.sprite.name);
       actualSegment.sprites.push(this.sprite);
 
-      const { trackSize } = window.tracks.f1Y91[this.road.trackName];
+      const { trackSize } = window.tracks.f1Y91[this.race.road.trackName];
       const actualPosition = this.trackPosition / 200;
       const actualLap = Math.floor(actualPosition / trackSize);
 
       if (this.lap < actualLap) {
-        this.raceTime.push(this.director.totalTime);
-        const raceLeader = this.director.positions[0].name;
+        this.raceTime.push(this.race.director.totalTime);
+        const raceLeader = this.race.director.positions[0].name;
         const leaderHasFinished = this.findFinishedCars(raceLeader)?.finished === true;
 
         if (leaderHasFinished) {
@@ -105,7 +147,7 @@ class Opponent {
         }
         this.lap += 1;
       }
-      if (this.lap > this.director.raceLaps) {
+      if (this.lap > this.race.director.raceLaps) {
         this.findFinishedCars(this.opponentName).finished = true;
       }
     }
